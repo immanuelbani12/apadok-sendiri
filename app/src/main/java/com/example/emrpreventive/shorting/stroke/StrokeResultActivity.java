438e1a2af569ff27821bb8c40cfab9646e6867c0
aulkiller <58225261+aulkiller@users.noreply.github.com>
2022-02-22 23:46:13 +0700

    App crash

--- app/src/main/java/com/example/emrpreventive/shorting/stroke/StrokeResultActivity.java
+++ app/src/main/java/com/example/emrpreventive/shorting/stroke/StrokeResultActivity.java
@@ -61,7 +61,7 @@ public class StrokeResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stroke_result);
+        setContentView(R.layout.activity_screening_result);
         setupItemView();
         setupJson();
     }
