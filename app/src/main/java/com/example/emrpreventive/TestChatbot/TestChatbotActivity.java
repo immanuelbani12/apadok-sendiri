e531cc996fed495d71ad28984020e8a395789bc0
iqbaalpratama <iqbaalpratama23@gmail.com>
2022-02-23 17:42:26 +0700

    add edit message for chatbot

--- app/src/main/java/com/example/emrpreventive/TestChatbot/TestChatbotActivity.java
+++ app/src/main/java/com/example/emrpreventive/TestChatbot/TestChatbotActivity.java
@@ -2,9 +2,12 @@ package com.example.emrpreventive.TestChatbot;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -34,9 +37,10 @@ import java.io.UnsupportedEncodingException;
 public class TestChatbotActivity extends AppCompatActivity {
 
     private Gson gson = new Gson();
-    private TextView tv_timeresult2, tv_btn_education, tv_btn_education2, tv_btn_education3, tv_timeresult4, tv_test, tv_test2, tv_test3, tv_test4, tv_test5;
     private Button btn_penjadwalan, btn_pagi, btn_siang, btn_sore;
+    private ImageButton btn_chat_send;
     private TextView tv_title_activity, tv_first_chat, tv_second_chat, tv_third_chat;
+    private EditText edit_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +59,8 @@ public class TestChatbotActivity extends AppCompatActivity {
         btn_pagi = (Button) findViewById(R.id.btn_pagi);
         btn_siang = (Button) findViewById(R.id.btn_siang);
         btn_sore = (Button) findViewById(R.id.btn_sore);
+        edit_message = (EditText) findViewById(R.id.edit_message);
+        btn_chat_send = (ImageButton) findViewById(R.id.btn_chat_send);
 
         Typeface helvetica_font = ResourcesCompat.getFont(getApplicationContext(),R.font.helvetica_neue);
         tv_title_activity.setTypeface(helvetica_font);
@@ -65,6 +71,11 @@ public class TestChatbotActivity extends AppCompatActivity {
         btn_pagi.setTypeface(helvetica_font);
         btn_siang.setTypeface(helvetica_font);
         btn_sore.setTypeface(helvetica_font);
+        btn_chat_send.setEnabled(false);
+        edit_message.setTypeface(helvetica_font);
+        edit_message.setEnabled(false);
+        edit_message.setInputType(InputType.TYPE_NULL);
+
 
         tv_second_chat.setVisibility(View.GONE);
         btn_pagi.setVisibility(View.GONE);
@@ -74,10 +85,6 @@ public class TestChatbotActivity extends AppCompatActivity {
     }
 
     public void show_question1(View view) {
-//        tv_test = (TextView) findViewById(R.id.time_result2);
-//        tv_test2 = (TextView) findViewById(R.id.btn_education);
-//        tv_test3 = (TextView) findViewById(R.id.btn_education2);
-//        tv_test4 = (TextView) findViewById(R.id.btn_education3);
         btn_penjadwalan.setEnabled(false);
         tv_second_chat.setVisibility(View.VISIBLE);
         btn_pagi.setVisibility(View.VISIBLE);
@@ -85,9 +92,6 @@ public class TestChatbotActivity extends AppCompatActivity {
         btn_sore.setVisibility(View.VISIBLE);
     }
     public void show_question2(View view) {
-//        tv_test3.setVisibility(View.GONE);
-//        tv_test4.setVisibility(View.GONE);
-//        tv_test5 = (TextView) findViewById(R.id.time_result4);
         btn_penjadwalan.setEnabled(false);
         btn_pagi.setEnabled(false);
         btn_siang.setEnabled(false);
@@ -96,9 +100,6 @@ public class TestChatbotActivity extends AppCompatActivity {
         tv_third_chat.setVisibility(View.VISIBLE);
     }
     public void show_question3(View view) {
-//        tv_test2.setVisibility(View.GONE);
-//        tv_test4.setVisibility(View.GONE);
-//        tv_test5 = (TextView) findViewById(R.id.time_result4);
         btn_penjadwalan.setEnabled(false);
         btn_pagi.setEnabled(false);
         btn_siang.setEnabled(false);
@@ -107,9 +108,6 @@ public class TestChatbotActivity extends AppCompatActivity {
         tv_third_chat.setVisibility(View.VISIBLE);
     }
     public void show_question4(View view) {
-//        tv_test2.setVisibility(View.GONE);
-//        tv_test3.setVisibility(View.GONE);
-//        tv_test5 = (TextView) findViewById(R.id.time_result4);
         btn_penjadwalan.setEnabled(false);
         btn_pagi.setEnabled(false);
         btn_siang.setEnabled(false);
